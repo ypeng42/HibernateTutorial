0242de49bc5a65d522195024551d398c811295cd
Yuqing Peng <yqpeng93@gmail.com>
2016-12-07 12:41:50 -0500

    add jdbc example

---
+++ src/main/java/com/hibernate/learn/JDBCExample.java
@@ -0,0 +1,69 @@
+package com.hibernate.learn;
+
+import java.sql.*;
+
+public class JDBCExample {
+    
+    // Note: naming for different DB (mysql, postgres, oracle, etc) are different, google yourself, should be easy
+    static final String JDBC_DRIVER = "org.postgresql.Driver";
+    static final String DB_URL = "jdbc:postgresql://localhost/testDB";
+
+    //  Database credentials
+    static final String USER = "postgres";
+    static final String PASS = "123";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT name, age FROM people";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while (rs.next()) {
+                //Retrieve by column name
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+
+                //Display values
+                System.out.print("Name: " + name);
+                System.out.print(", Age: " + age);
+            }
+            //STEP 6: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }
+}
